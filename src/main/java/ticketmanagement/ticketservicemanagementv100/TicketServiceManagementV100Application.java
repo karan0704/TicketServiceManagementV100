7150883020f5ac9077742f96a7150ae3a64de7af
Karan M <karanmohokar7@gmail.com>
2025-07-14 13:44:50 +0530

    Code Running

---
+++ src/main/java/ticketmanagement/ticketservicemanagementv100/TicketServiceManagementV100Application.java
@@ -0,0 +1,13 @@
+package ticketmanagement.ticketservicemanagementv100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketServiceManagementV100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketServiceManagementV100Application.class, args);
+	}
+
+}
---
+++ src/test/java/ticketmanagement/ticketservicemanagementv100/TicketServiceManagementV100ApplicationTests.java
@@ -0,0 +1,13 @@
+package ticketmanagement.ticketservicemanagementv100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceManagementV100ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
